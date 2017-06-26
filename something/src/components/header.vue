cd1cda4619d0238ea57b3dd7795c3898735c262f
king <justforgit@163.com>
2017-06-26 21:31:59 +0800

    new add

--- something/src/components/header.vue
+++ something/src/components/header.vue
@@ -1,11 +1,19 @@
 <template>
-	<div>
-		<el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-		  <el-menu-item index="1">Home</el-menu-item>
-		  <el-menu-item index="2">Tips</el-menu-item>
-		  <el-menu-item index="3">Demos</el-menu-item>
-        </el-menu>
-    </div>
+	<div class="flex j-cneter a-center nav-head">
+      <div class="logo">
+        <h1>Code Seeker</h1>
+      </div>
+      <div class="fg">
+       <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo flex" mode="horizontal" @select="handleSelect">
+         <el-menu-item index="1">Html</el-menu-item>
+         <el-menu-item index="2">Css</el-menu-item>
+         <el-menu-item index="3">Javascript</el-menu-item>
+         <el-menu-item index="4">Web tools</el-menu-item>
+         <el-menu-item index="5">Vue</el-menu-item>
+         <el-menu-item index="6">Others</el-menu-item>
+       </el-menu>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -17,19 +25,22 @@
     },
     methods: {
       handleSelect(key, keyPath) {
-        switch (key){
-        	case '1':
-        	this.$router.push({path:'/home'});
-        	break;
-        	case '2':
-        	this.$router.push({path:'/tips'});
-        	break;
-        	case '3':
-        	this.$router.push({path:'/demos'});
-        	break;
-        }
+        
       }
     }
   }
 </script>
+
+<style scoped lang="scss">
+  .nav-head{
+    background: #324157;
+    padding:0 100px;
+    .logo{
+       color:#fff;
+    }
+    .el-menu-demo{
+      justify-content: flex-end;
+    }
+  }
+</style>
 
