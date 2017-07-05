eacf79e81fe8325151071d40428384b5987f4d52
tangkaiqiang1 <justforgit@163.com>
2017-07-05 17:13:53 +0800

    es6 test

--- something/src/views/tips.vue
+++ something/src/views/tips.vue
@@ -1,9 +1,31 @@
 <template>
-<div>tips</div>
+    <div>
+        <el-button @click="test">test</el-button>
+    </div>
 </template>
 
 <script>
-    export default{
-        
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        test() {
+            // let {name,age} = {name:'king',age:12};
+
+            // var {name, child: {age}} = {name: "Bob", child: {age: 12}};
+            var students = [
+                { name: "Bob" },
+                { name: "Lily" }
+            ]
+
+            for (var { name } of students) {     //直接取出name属性
+                console.log(name);
+            }
+            // console.log(age);
+        }
     }
+}
 </script>
