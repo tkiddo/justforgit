adbdf6aa5170dce5f99b1bbb208f9bd70a6ae6cd
tangkaiqiang1 <justforgit@163.com>
2017-07-10 10:11:06 +0800

    stack

--- something/src/views/tips.vue
+++ something/src/views/tips.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <el-button @click="test">test</el-button>
+        <el-button @click="stack">stack</el-button>
     </div>
 </template>
 
@@ -25,6 +26,62 @@ export default {
                 console.log(name);
             }
             // console.log(age);
+        },
+        stack() {
+            function Stack() {
+                let items = [];
+                this.push = function (el) {
+                    items.push(el);
+                };
+                this.pop = function () {
+                    return items.pop();
+                };
+                this.peek = function () {
+                    return items[items.length - 1];
+                };
+                this.isEmpty = function () {
+                    return items.length == 0;
+                };
+                this.size = function () {
+                    return items.length;
+                };
+                this.clear = function () {
+                    items = [];
+                };
+                this.print = function () {
+                    console.log(items)
+                }
+            }
+
+            let stack = new Stack();
+            console.log(stack.isEmpty());
+            stack.push(9);
+            stack.push(45);
+            stack.pop();
+            stack.print();
+        },
+        queue() {
+            function Queue() {
+                let items = [];
+                this.enqueue = function (element) {
+                    items.push(element);
+                };
+                this.dequeue = function () {
+                    return items.shift();
+                };
+                this.front = function () {
+                    return items[0];
+                };
+                this.isEmpty = function () {
+                    return items.length == 0;
+                };
+                this.size = function () {
+                    return items.length;
+                };
+                this.print = function () {
+                    console.log(items.toString());
+                };
+            }
         }
     }
 }
