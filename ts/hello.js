aeaeb94ff4c90babf74a2c68ff124f871f9073df
tangkaiqiang1 <justforgit@163.com>
2017-08-14 09:03:30 +0800

    ts

---
+++ ts/hello.js
@@ -0,0 +1,32 @@
+function sayHello(person) {
+    return 'Hello,' + person;
+}
+let user = 'xfadf';
+let isDone = false;
+let boo = true;
+console.log(sayHello(user));
+let myName = 'car';
+let myAge = 24;
+let secentence = `hello ${myName},age is ${myAge}`;
+console.log(secentence);
+let a = 'seven';
+a = 'fasdf';
+let b = 'fsdfe';
+b = 2;
+let c = 'sdf';
+c = '3';
+let union = 'sdfs';
+union = 3;
+let liu = {
+    id: 1,
+    name: 'liu',
+    age: 23,
+    gender: 'male',
+    a: 111
+};
+console.log(liu);
+function sum(x, y) {
+    return x + y;
+}
+sum(1, 2);
+//# sourceMappingURL=hello.js.map
---
+++ ts/hello.ts
@@ -0,0 +1,54 @@
+function sayHello(person:string){
+    return 'Hello,'+person;
+
+}
+
+let user = 'xfadf';
+let isDone:boolean = false;
+let boo:boolean = true;
+console.log(sayHello(user));
+
+let myName:string = 'car';
+let myAge:number = 24;
+let secentence:string = `hello ${myName},age is ${myAge}`;
+console.log(secentence);
+
+let a:string = 'seven';
+a = 'fasdf';
+let b:any = 'fsdfe';
+b = 2;
+let c = 'sdf';
+c = '3';
+
+let union:string|number = 'sdfs';
+union = 3;
+
+interface Person{
+    readonly id:number;//只读属性
+    name:string;
+    age:number;
+    gender?:string;//可选属性
+    [propName:string]:any;//任意属性
+}
+
+let liu:Person = {
+    id:1,
+    name:'liu',
+    age:23,
+    gender:'male',
+    a:111
+};
+
+console.log(liu);
+
+
+
+function sum(x:number,y:number):number{
+    return x+y;
+}
+
+sum(1,2);
+
+let sum2:(x:number,y:number)=>number=function(x:number,y:number){
+    return x+y;
+}
