1001edf47a2d3ab03dfd8451b65d033b54903cdc
tangkaiqiang1 <justforgit@163.com>
2017-08-16 16:41:00 +0800

    dff

--- api/demo/blog/app.js
+++ api/demo/blog/app.js
@@ -9,16 +9,14 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1')
+    res.header("X-Powered-By", ' 3.2.1');
+    //将Access-Control-Allow-Credentials设为true
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Content-Type", "application/json;charset=utf-8");
     if (req.method == "OPTIONS") res.send(200); /*让options请求快速返回*/
     else next();
 });
 
-//处理post参数
-// 创建 application/x-www-form-urlencoded 编码解析
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // session
 var session = require('express-session');
@@ -26,6 +24,15 @@ var FileStore = require('session-file-store')(session);
 var identityKey = 'skey';
 var cookieParser = require('cookie-parser');
 var Favicon = require('serve-favicon');
+
+
+//处理post参数
+// 创建 application/x-www-form-urlencoded 编码解析
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 //session设置
 app.use(cookieParser())
 
